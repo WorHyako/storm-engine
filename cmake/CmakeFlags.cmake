314a4fd8542a93916b349c9df3cf4924484e9c18
WorHyako <worhyako@gmail.com>
2024-12-03 02:00:10 +0500

    replace core from 'libs' to 'modules'; up cxx standard to 20, coz smbody wanna use std::ranges

--- cmake/CmakeFlags.cmake
+++ cmake/CmakeFlags.cmake
@@ -3,6 +3,6 @@ cmake_minimum_required(VERSION 3.15)
 # TODO: delete it. Just for linker debugging.
 set(BUILD_SHARED_LIBS ON)
 
-set(CMAKE_CXX_STANDARD 17)
+set(CMAKE_CXX_STANDARD 20)
 set(CMAKE_CXX_STANDARD_REQUIRED ON)
 set(CMAKE_CXX_EXTENSIONS OFF)
